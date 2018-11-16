e1cab22ecb7a03a8b9d07e27cdf3d6a37ec314a1
sabeehah123 <sabeehah1996@gmail.com>
2018-11-16 10:24:27 +0000

    update

--- compute_resource.tf
+++ compute_resource.tf
@@ -1,6 +1,5 @@
 resource "google_compute_instance" "default" {
-<<<<<<< HEAD
-	name = "${var.name}"  
+	name = "${var.name}"
 	machine_type = "${var.machine_type}"
 	zone = "${var.zone}"
 	tags = ["${var.name}"]
@@ -9,56 +8,21 @@ resource "google_compute_instance" "default" {
 			image = "${var.image}"
 		}
 	}
-	network_interface { 
+	network_interface {
 		network = "${var.network}"
-=======
-	name = "jenkins"  
-	machine_type = "n1-standard-1"
-	zone = "europe-west2-c"
-	tags = ["jenkins"]
-	boot_disk {
-		initialize_params {
-			image = "centos-7"
-		}
-	}
-	network_interface { 
-		network = "default"
->>>>>>> 8941b0b751f8764b14d7688f7d4e9bf77f6fa516
 		access_config {
 			// Ephemeral IP
 		}
 	}
-
-	metadata { 
-<<<<<<< HEAD
-		sshKeys = "${var.ssh_user}:${file("${var.public_key}")}"
-	}
-	connection  = { 
+	metadata {
+    	sshKeys = "${var.ssh_user}:${file("${var.public_key}")}"
+  	}	
+	connection = {
 		type = "ssh"
-		user = "${var.ssh_user}" 
+		user = "${var.ssh_user}"
 		private_key = "${file("${var.private_key}")}"
-	} 
+	}
 	provisioner "remote-exec" {
-		scripts = [
-			"${var.scripts}" 
-=======
-		sshKeys = "terraform:${file("~/.ssh/id_rsa.pub")}"
+		scripts = "${var.scripts}"
 	}
-	connection  = { 
-		type = "ssh"
-		user = "terraform" 
-		private_key = "${file("~/.ssh/id_rsa")}"
-	}
-	provisioner "remote-exec" { 
-		inline = [
-			"sudo yum install -y java" 
-		]
-	} 
-	provisioner "remote-exec" {
-		scripts = [
-			"scripts/test1" 
->>>>>>> 8941b0b751f8764b14d7688f7d4e9bf77f6fa516
-		]
-	}
-
-} 
+}
